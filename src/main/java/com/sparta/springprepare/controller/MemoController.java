2bbe0646816764af856590d9bad0d34ded60049d
ironprayer <ironprayer1208@gmail.com>
2023-06-24 13:32:07 +0900

    update, delete 구현

--- src/main/java/com/sparta/springprepare/controller/MemoController.java
+++ src/main/java/com/sparta/springprepare/controller/MemoController.java
@@ -38,4 +38,26 @@ public class MemoController {
 
         return responseDtoList;
     }
+
+    @PutMapping("/memos/{id}")
+    public long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+        if(memoList.containsKey(id)){
+            Memo memo = memoList.get(id);
+
+            memo.update(requestDto);
+            return memo.getId();
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+
+    @DeleteMapping("/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        if(memoList.containsKey(id)) {
+            memoList.remove(id);
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다");
+        }
+    }
 }
