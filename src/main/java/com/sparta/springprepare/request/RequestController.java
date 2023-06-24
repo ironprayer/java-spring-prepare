41c2137914eb89ab2c91a2bf3400a045a6f347e0
ironprayer <ironprayer1208@gmail.com>
2023-06-24 10:37:19 +0900

    http 객체를 처리하는 방법

--- src/main/java/com/sparta/springprepare/request/RequestController.java
+++ src/main/java/com/sparta/springprepare/request/RequestController.java
@@ -1,5 +1,6 @@
 package com.sparta.springprepare.request;
 
+import com.sparta.springprepare.response.Star;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -38,5 +39,37 @@ public class RequestController {
     @ResponseBody
     public String helloPostRequestParam(@RequestParam String name, @RequestParam int age) {
         return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
+    }
+
+    // [Request sample]
+    // POST http://localhost:8080/hello/request/form/model
+    // Header
+    //  Content type: application/x-www-form-urlencoded
+    // Body
+    //  name=Robbie&age=95
+    @PostMapping("/form/model")
+    @ResponseBody
+    public String helloRequestBodyForm(@ModelAttribute Star star) {
+        return String.format("Hello, @ModelAttribute.<br> (name = %s, age = %d) ", star.name, star.age);
+    }
+
+    // [Request sample]
+    // GET http://localhost:8080/hello/request/form/param/model?name=Robbie&age=95
+    @GetMapping("/form/param/model")
+    @ResponseBody
+    public String helloRequestParam(@ModelAttribute Star star) {
+        return String.format("Hello, @ModelAttribute.<br> (name = %s, age = %d) ", star.name, star.age);
+    }
+
+    // [Request sample]
+    // POST http://localhost:8080/hello/request/form/json
+    // Header
+    //  Content type: application/json
+    // Body
+    //  {"name":"Robbie","age":"95"}
+    @PostMapping("/form/json")
+    @ResponseBody
+    public String helloPostRequestJson(@RequestBody Star star) {
+        return String.format("Hello, @RequestBody.<br> (name = %s, age = %d) ", star.name, star.age);
     }
 }
--- src/main/java/com/sparta/springprepare/response/Star.java
+++ src/main/java/com/sparta/springprepare/response/Star.java
@@ -5,8 +5,8 @@ import lombok.Setter;
 //@Getter
 @Setter
 public class Star {
-    String name;
-    int age;
+    public String name;
+    public int age;
 
     public Star(String name, int age) {
         this.name = name;
