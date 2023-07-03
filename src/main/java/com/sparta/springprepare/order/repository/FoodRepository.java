e681c2e7db1e899955668374ec175c243178ed3b
ironprayer <ironprayer1208@gmail.com>
2023-07-03 17:08:46 +0900

    OneToOne 단방향 테스트

---
+++ src/main/java/com/sparta/springprepare/order/repository/FoodRepository.java
@@ -0,0 +1,7 @@
+package com.sparta.springprepare.order.repository;
+
+import com.sparta.springprepare.order.entity.Food;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FoodRepository extends JpaRepository<Food, Long> {
+}
