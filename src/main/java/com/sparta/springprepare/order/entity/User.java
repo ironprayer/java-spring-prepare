d0fb0f859380868dcf7a4c491c7e94f03fbd2cfc
ironprayer <ironprayer1208@gmail.com>
2023-07-03 18:08:16 +0900

    ManyToMany 양방향 테스

--- src/main/java/com/sparta/springprepare/order/entity/User.java
+++ src/main/java/com/sparta/springprepare/order/entity/User.java
@@ -1,9 +1,10 @@
 package com.sparta.springprepare.order.entity;
-
-
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -14,4 +15,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @ManyToMany(mappedBy = "userList")
+    private List<Food> foodList = new ArrayList<>();
+
+    public void addFoodList(Food food) {
+        this.foodList.add(food);
+        food.getUserList().add(this); // 외래 키(연관 관계) 설정
+    }
 }
